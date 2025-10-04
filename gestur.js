46bce21a7115d8c9b0f2673d86067daed3e1ce3c
bastian <2404030073@students.unis.ac.id>
2025-10-04 19:33:34 +0700

    tambahan comment

--- gestur.js
+++ gestur.js
@@ -1,7 +1,9 @@
-gsap.from(".card", {
-  scrollTrigger: ".card",
+gsap.from('.card', {
+  scrollTrigger: '.card',
   y: 50,
   opacity: 0,
   duration: 1,
-  ease: "power3.out",
+  ease: 'power3.out',
 });
+
+// test perubahan untuk belajar push ke github
